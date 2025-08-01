1806780c659b03dc552177d1ee61a80315fb487f
Tomas Zahradnik <tomas.zahradnik@aimtecglobal.com>
2025-08-01 22:04:46 +0200

    Fix .vercelignore and simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,9 @@
-import {defineConfig, transformWithEsbuild} from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "tailwindcss";
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      name: 'treat-js-files-as-jsx',
-      async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/))  return null
-
-        // Use the exposed transform from vite, instead of directly
-        // transforming with esbuild
-        return transformWithEsbuild(code, id, {
-          loader: 'jsx',
-          jsx: 'automatic',
-        })
-      },
-    },
-      react(),
-    tailwindcss()],
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-
+  plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      }
-    }
   }
 })
